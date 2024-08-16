763d51f65c046bebca81e713ca6f408461620c2e
cayyanar <cayyanar@yahoo.co.in>
2024-08-15 22:36:44 -0500

    updated comment latest

--- packages/container/config/webpack.dev.js
+++ packages/container/config/webpack.dev.js
@@ -3,7 +3,7 @@ const { merge } = require ('webpack-merge');
 const commonConfig = require('./webpack.common');
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 
-//dependcies from this file
+
 const packageJson = require ('../package.json');
 
 
