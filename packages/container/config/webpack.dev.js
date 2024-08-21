34d6ca584ca5ce895bb29c6c9355a4c8aec8e129
cayyanar <cayyanar@yahoo.co.in>
2024-08-20 21:50:12 -0500

    updated s3 command

--- packages/container/config/webpack.dev.js
+++ packages/container/config/webpack.dev.js
@@ -3,7 +3,7 @@ const { merge } = require ('webpack-merge');
 const commonConfig = require('./webpack.common');
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 
-
+//added comment for build
 const packageJson = require ('../package.json');
 
 
