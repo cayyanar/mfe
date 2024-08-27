d09ee3eb81bd460746e6f36fb7e0b201bcd5a648
cayyanar <cayyanar@yahoo.co.in>
2024-08-26 22:39:03 -0500

    Added header js

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -2,11 +2,17 @@ import React from 'react';
 
 import MarketingApp from './components/MarketingApp';
 
-export default() => {
-    return <div>
-        <h1>Hi there !</h1>
-        <hr/>
-        <MarketingApp/>
+import Header from './components/Header';
+import { BrowserRouter } from 'react-router-dom/cjs/react-router-dom.min';
+
+export default () => {
+    return (
+    <BrowserRouter>
+      <div>
+        <Header />
+        <MarketingApp />
         
     </div>
+    </BrowserRouter>
+    );
 };
